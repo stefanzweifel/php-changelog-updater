a4ecbb5d6bbf1c7d2d3305c62eead48e16820423
Stefan Zweifel <hello@stefanzweifel.io>
2021-08-10 20:51:52 +0200

    Extract Code to an Action

--- app/Commands/UpdateCommand.php
+++ app/Commands/UpdateCommand.php
@@ -4,19 +4,8 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
-use App\CreateNewReleaseHeading;
-use App\FindPreviousVersionHeading;
-use App\FindUnreleasedHeading;
-use App\GenerateCompareUrl;
-use Illuminate\Support\Str;
+use App\Actions\AddReleaseNotesToChangelog;
 use LaravelZero\Framework\Commands\Command;
-use League\CommonMark\Environment\Environment;
-use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
-use League\CommonMark\Extension\CommonMark\Node\Inline\Link;
-use League\CommonMark\Node\Node;
-use League\CommonMark\Parser\MarkdownParser;
-use Wnx\CommonmarkMarkdownRenderer\MarkdownRendererExtension;
-use Wnx\CommonmarkMarkdownRenderer\Renderer\MarkdownRenderer;
 
 class UpdateCommand extends Command
 {
@@ -31,7 +20,7 @@ class UpdateCommand extends Command
 
     protected $description = 'Update Changelog with the given release notes';
 
-    public function handle(CreateNewReleaseHeading $createNewReleaseHeading, FindUnreleasedHeading $findUnreleasedHeading, FindPreviousVersionHeading $findPreviousVersionHeading, GenerateCompareUrl $generateCompareUrl)
+    public function handle(AddReleaseNotesToChangelog $addReleaseNotesToChangelog)
     {
         $releaseNotes = $this->option('release-notes');
         $repositoryUrl = $this->option('repository');
@@ -42,39 +31,13 @@ class UpdateCommand extends Command
 
         $changelog = $this->getChangelogContent($pathToChangelog);
 
-        $environment = new Environment();
-        $environment->addExtension(new MarkdownRendererExtension());
-
-        $parser = new MarkdownParser($environment);
-        $markdownRenderer = new MarkdownRenderer($environment);
-
-        $originalChangelog = $parser->parse($changelog);
-        $parsedReleaseNotes = $parser->parse($releaseNotes);
-
-
-        $unreleasedHeading = $findUnreleasedHeading->find($originalChangelog);
-        $previousVersion = $this->getPreviousVersionFromUnreleasedHeading($unreleasedHeading);
-        $updatedUrl = $generateCompareUrl->generate($repositoryUrl, $latestVersion, 'HEAD');
-
-        $this->updateUrlOnUnreleasedHeading($unreleasedHeading, $updatedUrl);
-
-
-        // Create new Heading containing the new version number
-        $newReleaseHeading = $createNewReleaseHeading->create($repositoryUrl, $previousVersion, $latestVersion, $releaseDate);
-
-        // Prepend the new Release Heading to the Release Notes
-        $parsedReleaseNotes->prependChild($newReleaseHeading);
-
-
-        // Find the Heading of the previous Version
-        $previousVersionHeading = $findPreviousVersionHeading->find($originalChangelog, $previousVersion);
-
-        // Insert the newest Release Notes before the previous Release Heading
-        $previousVersionHeading->insertBefore($parsedReleaseNotes);
-
-
-        // Render Document to Markdown
-        $updatedMarkdown = $markdownRenderer->renderDocument($originalChangelog);
+        $updatedMarkdown = $addReleaseNotesToChangelog->execute(
+            changelog: $changelog,
+            releaseNotes: $releaseNotes,
+            latestVersion: $latestVersion,
+            releaseDate: $releaseDate,
+            repositoryUrl: $repositoryUrl
+        );
 
         $this->info($updatedMarkdown->getContent());
 
@@ -83,40 +46,8 @@ class UpdateCommand extends Command
         }
     }
 
-    /**
-     * @param Node|null $matchingNodes
-     */
-    private function updateUrlOnUnreleasedHeading(?Node $matchingNodes, string $url): void
-    {
-        /** @var Link $link */
-        $link = $matchingNodes->firstChild();
-        $link->setUrl($url);
-    }
-
-    /**
-     * @param string $pathToChangelog
-     * @return false|string
-     */
-    protected function getChangelogContent(string $pathToChangelog)
+    protected function getChangelogContent(string $pathToChangelog): bool|string
     {
         return file_get_contents($pathToChangelog);
-    }
-
-    /**
-     * @param Heading $unreleasedHeading
-     * @return string
-     * @throws \Throwable
-     */
-    private function getPreviousVersionFromUnreleasedHeading(Heading $unreleasedHeading): string
-    {
-        /** @var Link $linkNode */
-        $linkNode = $unreleasedHeading->firstChild();
-
-        throw_if($linkNode === null, new \LogicException("Can not find link node in unreleased heading."));
-
-        return Str::of($linkNode->getUrl())
-            ->afterLast('/')
-            ->explode('...')
-            ->first();
     }
 }
