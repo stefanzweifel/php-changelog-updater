95aa21ed1b2f29c09117430dbafaa446c9db260c
Stefan Zweifel <hello@stefanzweifel.io>
2021-08-10 20:35:07 +0200

    Add FindPreviousVersionHeading

---
+++ app/FindPreviousVersionHeading.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\QueryExpressions\HeadingLevel;
+use App\QueryExpressions\HeadingText;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Link;
+use League\CommonMark\Node\Block\Document;
+use League\CommonMark\Node\Inline\Text;
+use League\CommonMark\Node\Node;
+use League\CommonMark\Node\Query;
+
+class FindPreviousVersionHeading
+{
+    /**
+     * @param Document $document
+     * @return Node|null
+     */
+    public function find(Document $document, string $previousVersion): ?Node
+    {
+        return (new Query())
+            ->where(Query::type(Heading::class))
+            ->andWhere(new HeadingLevel(2))
+            ->andWhere(new HeadingText($previousVersion))
+            ->findOne($document);
+    }
+}
---
+++ tests/Unit/FindPreviousVersionHeadingTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\FindPreviousVersionHeading;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Parser\MarkdownParser;
+
+it('finds previous version heading in markdown ast', function () {
+    $markdown = <<<MD
+    ## [v1.0.0](http://example.com)
+    MD;
+
+    $env = new Environment();
+    $env->addExtension(new CommonMarkCoreExtension());
+    $parser = new MarkdownParser($env);
+
+    $ast = $parser->parse($markdown);
+
+    $result = app(FindPreviousVersionHeading::class)->find($ast, 'v1.0.0');
+
+    $this->assertNotNull($result);
+    $this->assertInstanceOf(Heading::class, $result);
+});
+
+
+it('returns null if no previous version heading can be found in the markdown ast', function () {
+    $markdown = <<<MD
+    There is no heading here.
+    MD;
+
+    $env = new Environment();
+    $env->addExtension(new CommonMarkCoreExtension());
+    $parser = new MarkdownParser($env);
+
+    $ast = $parser->parse($markdown);
+
+    $result = app(FindPreviousVersionHeading::class)->find($ast, 'v1.0.0');
+
+    $this->assertNull($result);
+});
