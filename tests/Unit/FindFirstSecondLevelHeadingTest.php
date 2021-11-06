4dbe746b7efecaad69452f7c3c7fe719c77fa497
Stefan Zweifel <hello@stefanzweifel.io>
2021-11-06 16:51:16 +0100

    Add Test

---
+++ tests/Unit/FindFirstSecondLevelHeadingTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\FindFirstSecondLevelHeading;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Parser\MarkdownParser;
+
+it('finds first second level heading in markdown ast', function () {
+    $markdown = <<<MD
+    ## v1.1.0
+    ## v1.0.0
+    MD;
+
+    $env = new Environment();
+    $env->addExtension(new CommonMarkCoreExtension());
+    $parser = new MarkdownParser($env);
+
+    $ast = $parser->parse($markdown);
+
+    $result = app(FindFirstSecondLevelHeading::class)->find($ast);
+
+    $this->assertNotNull($result);
+    $this->assertInstanceOf(Heading::class, $result);
+
+    $this->assertEquals('v1.1.0', $result->firstChild()->getLiteral());
+});
+
+it('finds first second level heading in markdown ast if heading contains a link', function () {
+    $markdown = <<<MD
+    ## [v1.1.0](http://example.com)
+    ## [v1.0.0](http://example.com)
+    MD;
+
+    $env = new Environment();
+    $env->addExtension(new CommonMarkCoreExtension());
+    $parser = new MarkdownParser($env);
+
+    $ast = $parser->parse($markdown);
+
+    /** @var Heading $result */
+    $result = app(FindFirstSecondLevelHeading::class)->find($ast);
+
+    $this->assertNotNull($result);
+    $this->assertInstanceOf(Heading::class, $result);
+
+    $this->assertEquals('v1.1.0', $result->firstChild()->firstChild()->getLiteral());
+});
