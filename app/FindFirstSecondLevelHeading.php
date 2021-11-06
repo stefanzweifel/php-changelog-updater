f84f89a5518dd0a74d72224114c65bf160e298cc
Stefan Zweifel <hello@stefanzweifel.io>
2021-11-06 16:42:38 +0100

    Paste Notes at the top if no Unreleased Heading is found

---
+++ app/FindFirstSecondLevelHeading.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\QueryExpressions\HeadingLevel;
+use App\QueryExpressions\HeadingText;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Node\Block\Document;
+use League\CommonMark\Node\Node;
+use League\CommonMark\Node\Query;
+
+class FindFirstSecondLevelHeading
+{
+    /**
+     * @param Document $document
+     * @return Node|null
+     */
+    public function find(Document $document): ?Node
+    {
+        return (new Query())
+            ->where(Query::type(Heading::class))
+            ->andWhere(new HeadingLevel(2))
+            ->findOne($document);
+    }
+}
