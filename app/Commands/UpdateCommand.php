282723e7044e0b95b687422d2559e0cd00982ad4
Stefan Zweifel <hello@stefanzweifel.io>
2021-11-07 09:54:14 +0100

    Rearrange Code in UpdateCommand

--- app/Commands/UpdateCommand.php
+++ app/Commands/UpdateCommand.php
@@ -15,7 +15,7 @@ class UpdateCommand extends Command
     protected $signature = 'update
         {--release-notes= : Markdown Release Notes to be added to the CHANGELOG}
         {--latest-version= : The version the CHANGELOG should be updated too}
-        {--release-date= : Date when latest version has been released}
+        {--release-date= : Date when latest version has been released. Defaults to today.}
         {--path-to-changelog=CHANGELOG.md : Path to changelog markdown file to be updated}
         {-w\--write : Write changes to file}
     ';
@@ -33,7 +33,6 @@ class UpdateCommand extends Command
         $latestVersion = $this->option('latest-version');
         $releaseDate = $this->option('release-date');
         $pathToChangelog = $this->option('path-to-changelog');
-        $shouldWriteToFile = $this->option('write');
 
         if (empty($releaseDate)) {
             $releaseDate = now()->format('Y-m-d');
@@ -50,7 +49,13 @@ class UpdateCommand extends Command
 
         $this->info($updatedChangelog->getContent());
 
-        $this->writeChangelogToFile($shouldWriteToFile, $pathToChangelog, $updatedChangelog);
+        $this->writeChangelogToFile($pathToChangelog, $updatedChangelog);
+    }
+
+    private function validateOptions(): void
+    {
+        Assert::stringNotEmpty($this->option('release-notes'), 'No release-notes option provided. Abort.');
+        Assert::stringNotEmpty($this->option('latest-version'), 'No latest-version option provided. Abort.');
     }
 
     protected function getChangelogContent(string $pathToChangelog): bool | string
@@ -58,16 +63,10 @@ class UpdateCommand extends Command
         return file_get_contents($pathToChangelog);
     }
 
-    protected function writeChangelogToFile(bool $shouldWriteToFile, string $pathToChangelog, RenderedContentInterface $updatedMarkdown): void
+    protected function writeChangelogToFile(string $pathToChangelog, RenderedContentInterface $updatedMarkdown): void
     {
-        if ($shouldWriteToFile) {
+        if ($this->option('write')) {
             file_put_contents($pathToChangelog, $updatedMarkdown->getContent());
         }
-    }
-
-    private function validateOptions(): void
-    {
-        Assert::stringNotEmpty($this->option('release-notes'), 'No release-notes option provided. Abort.');
-        Assert::stringNotEmpty($this->option('latest-version'), 'No latest-version option provided. Abort.');
     }
 }
