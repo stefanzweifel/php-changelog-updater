01b7365d264c0f6664c7ecc870bbfdd36fc0ffe4
Stefan Zweifel <hello@stefanzweifel.io>
2022-03-15 19:45:01 +0100

    Update Exception Message

--- app/Exceptions/ReleaseAlreadyExistsInChangelogException.php
+++ app/Exceptions/ReleaseAlreadyExistsInChangelogException.php
@@ -8,6 +8,6 @@ class ReleaseAlreadyExistsInChangelogException extends Exception
 {
     public function __construct(public string $release)
     {
-        parent::__construct();
+        parent::__construct("CHANGELOG was not updated as release notes for {$release} already exist.");
     }
 }
