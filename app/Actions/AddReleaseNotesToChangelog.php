9a28bca8c00635b9c51071a1b70615223b01b696
Stefan Zweifel <hello@stefanzweifel.io>
2022-03-15 19:47:18 +0100

    Simplify Code

--- app/Actions/AddReleaseNotesToChangelog.php
+++ app/Actions/AddReleaseNotesToChangelog.php
@@ -7,7 +7,7 @@ namespace App\Actions;
 use App\Exceptions\ReleaseAlreadyExistsInChangelogException;
 use App\MarkdownParser;
 use App\MarkdownRenderer;
-use App\Queries\FindSecondLevelHeading;
+use App\Queries\FindPreviousVersionHeading;
 use App\Queries\FindUnreleasedHeading;
 use League\CommonMark\Node\Block\Document;
 use League\CommonMark\Output\RenderedContentInterface;
@@ -64,7 +64,7 @@ class AddReleaseNotesToChangelog
 
     private function checkIfVersionAlreadyExistsInChangelog(Document $changelog, string $latestVersion): void
     {
-        $result = app(FindSecondLevelHeading::class)->find($changelog, $latestVersion);
+        $result = app(FindPreviousVersionHeading::class)->find($changelog, $latestVersion);
 
         throw_unless(is_null($result), new ReleaseAlreadyExistsInChangelogException($latestVersion));
     }
