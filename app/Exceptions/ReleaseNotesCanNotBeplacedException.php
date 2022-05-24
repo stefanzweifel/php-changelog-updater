f7ceb2b27632f4334916bee48761071f8948ebd0
Stefan Zweifel <hello@stefanzweifel.io>
2022-05-24 19:52:40 +0200

    Add ReleaseNotesCanNotBeplacedException

---
+++ app/Exceptions/ReleaseNotesCanNotBeplacedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class ReleaseNotesCanNotBeplacedException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct("Release notes could not be placed. Is the CHANGELOG empty? Does it contain at least one heading?");
+    }
+}
