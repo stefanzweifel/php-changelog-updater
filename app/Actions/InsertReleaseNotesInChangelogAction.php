39f965c5190ebe61b7d7ba19eeae7f4760dd3e5e
Stefan Zweifel <hello@stefanzweifel.io>
2022-05-28 14:52:07 +0200

    Call PrepareReleaseNotes inside InsertReleaseNotes Action

--- app/Actions/InsertReleaseNotesInChangelogAction.php
+++ app/Actions/InsertReleaseNotesInChangelogAction.php
@@ -10,22 +10,31 @@ use League\CommonMark\Node\Block\Document;
 
 class InsertReleaseNotesInChangelogAction
 {
+    public function __construct(private PrepareReleaseNotesAction $prepareReleaseNotes)
+    {
+    }
+
     /**
      * @throws ReleaseNotesCanNotBeplacedException
      */
-    public function execute(Document $changelog, Document $releaseNotes, ?Heading $previousVersionHeading): Document
+    public function execute(Document $changelog, string $releaseNotes, Heading $newReleaseHeading, ?Heading $previousVersionHeading): Document
     {
+        // Prepare raw release notes to be inserted into CHANGELOG.
+        $parsedReleaseNotes = $this->prepareReleaseNotes->execute($releaseNotes, $newReleaseHeading);
+
         // If heading for a previous release exists, insert new release
         // notes block **before** the previous release heading.
         if ($previousVersionHeading !== null) {
-            $previousVersionHeading->insertBefore($releaseNotes);
+            $previousVersionHeading->insertBefore($parsedReleaseNotes);
+
             return $changelog;
         }
 
         // If no previous version heading exists in the document, we consider the CHANGELOG empty.
         // Insert the release notes at the end of the document (after the last element in the existing CHANGELOG).
         if ($changelog->lastChild() !== null) {
-            $changelog->lastChild()->insertAfter($releaseNotes);
+            $changelog->lastChild()->insertAfter($parsedReleaseNotes);
+
             return $changelog;
         }
 
