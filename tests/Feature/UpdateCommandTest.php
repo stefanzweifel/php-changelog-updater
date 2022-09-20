22981a6eeee7fd5fbf8a197b2195b2dec8f159e0
Amaljith K <amaljith.k@bigbinary.com>
2022-09-20 12:33:42 +0530

    Updated tests

--- tests/Feature/UpdateCommandTest.php
+++ tests/Feature/UpdateCommandTest.php
@@ -16,6 +16,7 @@ it('places given release notes in correct position in given markdown changelog',
         - Remove Feature D
         MD,
         '--latest-version' => 'v1.0.0',
+        '--latest-commit' => '1eefb4b7adef74e1b21c336063fbb8071f0c6e6f',
         '--path-to-changelog' => __DIR__ . '/../Stubs/base-changelog.md',
         '--release-date' => '2021-02-01',
     ])
@@ -71,6 +72,7 @@ it('uses current date for release date if no option is provieded', function () {
         - Remove Feature D
         MD,
         '--latest-version' => 'v1.0.0',
+        '--latest-commit' => '1eefb4b7adef74e1b21c336063fbb8071f0c6e6f',
         '--path-to-changelog' => __DIR__ . '/../Stubs/base-changelog.md',
     ])
          ->expectsOutput($expectedOutput)
@@ -94,6 +96,7 @@ it('uses current date for release date if option is empty', function () {
         - Remove Feature D
         MD,
         '--latest-version' => 'v1.0.0',
+        '--latest-commit' => '1eefb4b7adef74e1b21c336063fbb8071f0c6e6f',
         '--path-to-changelog' => __DIR__ . '/../Stubs/base-changelog.md',
         '--release-date' => '',
     ])
@@ -157,6 +160,7 @@ it('places given release notes in correct position even if changelog is empty be
         - Remove Feature D
         MD,
         '--latest-version' => 'v1.0.0',
+        '--latest-commit' => '1eefb4b7adef74e1b21c336063fbb8071f0c6e6f',
         '--path-to-changelog' => __DIR__ . '/../Stubs/base-changelog-empty-with-unreleased.md',
         '--release-date' => '2021-02-01',
     ])
@@ -178,6 +182,7 @@ it('uses compare-url-target option in unreleased heading url', function () {
         - Remove Feature D
         MD,
         '--latest-version' => 'v1.0.0',
+        '--latest-commit' => '1eefb4b7adef74e1b21c336063fbb8071f0c6e6f',
         '--path-to-changelog' => __DIR__ . '/../Stubs/base-changelog-with-custom-compare-url-target.md',
         '--release-date' => '2021-02-01',
         '--compare-url-target-revision' => '1.x',
--- tests/Stubs/expected-changelog.md
+++ tests/Stubs/expected-changelog.md
@@ -5,12 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [Unreleased](https://github.com/org/repo/compare/v1.0.0...HEAD)
+## [Unreleased](https://github.com/org/repo/compare/1eefb4b7adef74e1b21c336063fbb8071f0c6e6f...HEAD)
 
 Please do not update the unreleased notes.
 
 <!-- Content should be placed here -->
-## [v1.0.0](https://github.com/org/repo/compare/v0.1.0...v1.0.0) - 2021-02-01
+## [v1.0.0](https://github.com/org/repo/compare/v0.1.0...1eefb4b7adef74e1b21c336063fbb8071f0c6e6f) - 2021-02-01
 
 ### Added
 
