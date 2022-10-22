3eaa107f394c16e4bef5311993de66d7ba01c842
Stefan Zweifel <hello@stefanzweifel.io>
2022-10-22 21:00:35 +0200

    Prepare Tests for GITHUB_OUTPUT

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -8,6 +8,8 @@ use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
 {
+    public readonly string $gitHubOutputTestfile;
+
     /**
      * Creates the application.
      *
@@ -17,8 +19,18 @@ trait CreatesApplication
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
+        $this->gitHubOutputTestfile = base_path('tests/github_output.txt');
+
+        // Can be enabled by default, once the old set-output syntax has been removed.
+        // $this->hasGitHubOutputEnvironment();
+
         $app->make(Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    public function hasGitHubOutputEnvironment()
+    {
+        putenv("GITHUB_OUTPUT=$this->gitHubOutputTestfile");
     }
 }
