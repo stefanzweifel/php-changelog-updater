b260c87a310aaa4bfee411ab48279f30cd6060da
stefanzweifel <stefanzweifel@users.noreply.github.com>
2023-04-16 10:08:00 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -26,7 +26,7 @@ uses(Tests\TestCase::class)->in('Feature');
 |
 */
 
-expect()->extend('toBeOne', fn() => $this->toBe(1));
+expect()->extend('toBeOne', fn () => $this->toBe(1));
 
 /*
 |--------------------------------------------------------------------------
