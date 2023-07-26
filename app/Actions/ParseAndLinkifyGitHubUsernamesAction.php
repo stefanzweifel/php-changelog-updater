03e72875f2f1de4a1f287716e41f2a7b125d5258
Lu Fei <52o@qq52o.cn>
2023-07-26 14:53:35 +0800

    Fix get github username pattern (#43)
    * Fix get github username pattern
    
    * Add test

--- app/Actions/ParseAndLinkifyGitHubUsernamesAction.php
+++ app/Actions/ParseAndLinkifyGitHubUsernamesAction.php
@@ -16,9 +16,9 @@ class ParseAndLinkifyGitHubUsernamesAction
         // - `(?<!\[)` and `(?!\])` are negative lookbehind and lookahead assertions, respectively.
         //   They ensure that the GitHub username is not preceded or followed by a square
         //   bracket [ or ], which indicates that the username is already wrapped in a link.
-        // - `@([A-Za-z0-9_]+)` matches the GitHub username itself. It starts with
+        // - `@([A-Za-z0-9_-]+)` matches the GitHub username itself. It starts with
         //   the @ symbol and consists of alphanumeric characters and underscores.
-        $pattern = '/(?<!\[)@([A-Za-z0-9_]+)(?!\])/';
+        $pattern = '/(?<!\[)@([A-Za-z0-9_-]+)(?!\])/';
 
         $replacement = '[@$1](https://github.com/$1)';
 
