08a8e56b3f6ce1d3849e1d75811a70b8817a747f
Stefan Zweifel <stefan@stefanzweifel.dev>
2024-06-08 13:59:06 +0200

    Don't add Extension in ServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,8 +6,6 @@ namespace App\Providers;
 
 use App\Support\GitHubActionsOutput;
 use Illuminate\Support\ServiceProvider;
-use League\CommonMark\Environment\Environment;
-use League\CommonMark\Extension\Mention\MentionExtension;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,27 +17,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->singleton(GitHubActionsOutput::class, fn () => new GitHubActionsOutput());
-
-        $this->app->bind(Environment::class, function () {
-            $config = [
-                'mentions' => [
-                    // GitHub handler mention configuration.
-                    // Sample Input:  `@colinodell`
-                    // Sample Output: `<a href="https://www.github.com/colinodell">@colinodell</a>`
-                    'github_handle' => [
-                        'prefix' => '@',
-                        'pattern' => '[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)',
-                        'generator' => 'https://github.com/%s',
-                    ],
-                ],
-            ];
-
-            $environment = new Environment($config);
-
-            $environment->addExtension(new MentionExtension());
-
-            return $environment;
-        });
     }
 
     /**
